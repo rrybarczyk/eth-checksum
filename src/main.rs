87554669b13199bc27e5becbe595dff54f44811b
Casey Rodarmor <casey@rodarmor.com>
2018-08-13 22:57:22 -0700

    Suggestions

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 extern crate sha3;
-extern crate regex;
 
 use sha3::{Digest, Keccak256};
-use regex::Regex;
 
 fn main() {
     // Define address, should pass in via cmd
     let addr = "0x4B0897b0513fdC7C541B6d9D7E929C4e5364D2dB";
+
     // Add addr validity checks
 
     // Remove "0x" prefix if exists and make everything lowercase
@@ -20,9 +19,24 @@ fn main() {
 }
 
 // Remove "0x" prefix
-fn strip_0x(addr: &str) -> String {
-    let re = Regex::new(r"[0][x]").unwrap();
-    return re.replace_all(addr, "").to_string();
+//
+// NB. Returning a &str here avoids an extra allocation.
+//     The returned string will be a substring of the input.
+//
+// NB. The previous regex was `Regex::new(r"[0][x]")`,
+//     but Regex::new("0x") would have had the same effect.
+//     However, both were incorrect. Rust regexes match
+//     anywhere in a string, you would need `^0x` to only
+//     match the beginning of the string.
+fn strip_0x(addr: &str) -> &str {
+    // NB. explicit returns aren't needed if the value you
+    //     wish to return is the last statment of the function
+    //     or closure
+    if &addr[0..2] == "0x" {
+        &addr[2..]
+    } else {
+        addr
+    }
 }
 
 // Take keccak256 of formatted address
@@ -38,32 +52,37 @@ fn hash_addr(lc_addr: &str) -> String {
 
     // Get vector of bytes in hex form
     // Iterate for each byte, format each to hex string with map, collect into vector
-    let hex_vec: Vec<String> = addr_hash.iter()
+    //
+    // NB. Switched from type annotation to turbofish.
+    //     Both are the same here, but sometimes a
+    //     turbofish (which definitely looks like a
+    //     fish: ::<>) is useful, since you can chain
+    //     them.
+    let hex_vec = addr_hash.iter()
         .map(|b| format!("{:02X}", b))
-        .collect();
+        .collect::<Vec<String>>();
 
     // Join each hex byte representation in vector into single hex string and return
-    return hex_vec.join("");
+    //
+    // NB. Removed explicit return.
+    hex_vec.join("")
 }
 
 fn checksum(addr: &str, addr_hash: &str) -> String {
-
     // Define new empty string to hold checksum addr
     let mut checksum_addr = String::new();
 
-    // Access each character in addr hash
-    let mut addr_hash_chars = addr_hash.chars();
-
-    for (i, c) in addr.chars().enumerate() {
-        // Get hash char to evaluate
-        let mut hash_char = addr_hash_chars.next().unwrap();
-
+    // NB. Switched to zip of both char iterators
+    for (c, hash_char) in addr.chars().zip(addr_hash.chars()) {
         // If ith hash char is greater than 8, capitilize ith addr char
         if hash_char.to_digit(15) > Some(8) {
             checksum_addr.extend(c.to_uppercase());
         } else {
-            checksum_addr.insert(i, c);
+            // NB. Changed to `push` which appends to the end of the String
+            checksum_addr.push(c);
         }
     }
-    return checksum_addr;
+
+    // NB. removed explicit return
+    checksum_addr
 }
